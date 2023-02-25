761933621dd3e1ea3eced4f482eea7993459607d
AlexBrown4020 <balex3497@gmail.com>
2023-02-25 11:46:09 +0900

    Feat: Remove unused controllers folder

--- api/models/admin.js
+++ api/models/admin.js
@@ -14,10 +14,6 @@ const AdminSchema = new mongoose.Schema({
         type:String,
         required: true
     },
-    photo:{
-        type:String,
-        default:''
-    },
     isAdmin:{
         type:Boolean,
         default: false,
